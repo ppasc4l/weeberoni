775827556fb6d78210700e3087e00ac204b63eb5
ppasc4l <pasc4l17@gmail.com>
2016-12-14 15:57:34 -0600

    Add files via upload

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\why$/, botRegex2 = /^\cat$/;
+      botRegex = /^\why$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -16,32 +16,7 @@ function respond() {
     this.res.writeHead(200);
     this.res.end();
   }
-  
-  if(request.text && botRegex2.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage2();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
-}
 
-function postMessage2() {
-	var botResp, option, bod, botR;
-	
-	botResp = "http://i.imgur.com/2Uqlxkd.jpg";
-	
-	options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-	};
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
 }
 
 function postMessage() {
