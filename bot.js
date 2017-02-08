310660f52635f5096259d459277993df23aac122
ppasc4l <pasc4l17@gmail.com>
2017-02-08 17:38:31 -0600

    Add files via upload

--- bot.js
+++ bot.js
@@ -155,12 +155,6 @@ function postMessage() {
 		botResponse = "Flintstones, Meet the Flintstones";
 		break;
   }
-  
-  function tendiesFunc() {
-	  var botResponse;
-	  
-	  botResponse = "https://www.youtube.com/watch?v=7ERzPFCE7B0";
-  }
 
   options = {
     hostname: 'api.groupme.com',
@@ -192,5 +186,39 @@ function postMessage() {
   botReq.end(JSON.stringify(body));
 }
 
+function tendiesFunc() {
+	  var botResponse;
+	  
+	  botResponse = "https://www.youtube.com/watch?v=7ERzPFCE7B0";
+	  
+	options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+	};
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+  }
 
 exports.respond = respond;
