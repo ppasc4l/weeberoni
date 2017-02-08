0a4665228a7f571f62f78f0bed207b8d32f86d9f
ppasc4l <pasc4l17@gmail.com>
2017-02-08 16:54:42 -0600

    Add files via upload

--- bot.js
+++ bot.js
@@ -5,8 +5,9 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\gymsock$/;
+      botRegex = /^\GYMSOCK$/;
 
+	  request = request.toUpperCase();
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
